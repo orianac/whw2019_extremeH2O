fe4c8ff57b1a59ebee440c9aeb42ece193c24588
Amy De Luna <amydeluna@D-10-18-194-85.dhcp4.washington.edu>
2019-03-28 13:28:52 -0700

    added function to calculate longest consecutive days of hi/low flow

--- lowflows/analysis_functions.py
+++ lowflows/analysis_functions.py
@@ -22,3 +22,21 @@ def count_annual_days_below_threshold(timeseries, threshold):
 
 def aug_sept(month):
     return (month>=8) & (month<=9)
+
+def consecutive_days(timeseries):
+    '''
+    Inputs: 
+    - timeseries: a timeseries of hi/low flows - can use get_quantile_flow to get this
+    Outputs:
+    - the longest length of consecutive days of hi/low flow
+    '''
+    flow_dates = timeseries.time
+    # spacing between each hi/low flow date:
+    differences = flow_dates.diff('time')
+    # length of one day:
+    delta = np.timedelta64(86400000000000,'ns')
+    # filter out hi/low flow periods greater than 1 day
+    greatest_ind = np.where(differences > delta)[0]
+    # return the longest number of consecutive days of hi/low flow
+    return np.diff(greatest_ind).max()-1
+
